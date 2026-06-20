624a497124dbb33dd2d8cd7c0901077edc83636e
ajitesh <ajitesh.senjai@gmail.com>
2026-06-19 23:05:53 -0500

    prime

---
+++ src/main/java/checkPrime.java
@@ -0,0 +1,20 @@
+public class checkPrime {
+    public static void main (String [] args) {
+        int inputNo,m;
+        boolean p;
+        inputNo = 10856493;
+        p = true;
+        for (int x=2;x<=inputNo-1;x++) {
+            m = inputNo%x;
+            if (m==0) {
+                p = false;
+                break;
+            }
+        }
+        if (p) {
+            System.out.println("Your number "+inputNo+" is prime.");
+        } else {
+            System.out.println("Your number "+inputNo+" is not prime.");
+        }
+    }
+}
