750682ceaf741c76703235de75b54b6d73f0f12a
ajitesh <ajitesh.senjai@gmail.com>
2026-06-19 23:10:59 -0500

    Testing

---
+++ src/main/java/Testing.java
@@ -0,0 +1,14 @@
+public class Testing {
+
+    public static void main () {
+        int [][] a = new int[51][3];
+
+        for (int i = 0;i <=50;i++) {
+            a[i][0] = 5;
+            a[i][1] = i;
+            a[i][2] = a[i][0] * a[i][1];
+            System.out.println(a[i][0] + " x " + a[i][1] + " = " + a[i][2]);
+        }
+    }
+
+}
