e595c10f75eb57e7412b410e2d9c2e031ee39875
ajitesh <ajitesh.senjai@gmail.com>
2026-06-19 23:00:52 -0500

    array2d

--- src/main/java/array2d.java
+++ src/main/java/array2d.java
@@ -1,5 +1,5 @@
 public class array2d {
-    public static void main (String[] args) {
+    public static void main () {
         int a[][] = new int[51][3];
 
         for (int i = 0;i <=50;i++) {
@@ -7,6 +7,6 @@ public class array2d {
             a[i][1] = i;
             a[i][2] = a[i][0] * a[i][1];
             System.out.println(a[i][0] + " x " + a[i][1] + " = " + a[i][2]);
-        } 
+        }
     }
 }
