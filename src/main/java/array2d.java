6bfc89295d98f888b09276a406528f54e5039ba3
ajitesh <ajitesh.senjai@gmail.com>
2026-06-19 22:59:13 -0500

    array2d

--- src/main/java/array2d.java
+++ src/main/java/array2d.java
@@ -7,6 +7,6 @@ public class array2d {
             a[i][1] = i;
             a[i][2] = a[i][0] * a[i][1];
             System.out.println(a[i][0] + " x " + a[i][1] + " = " + a[i][2]);
-        }
+        } 
     }
 }
